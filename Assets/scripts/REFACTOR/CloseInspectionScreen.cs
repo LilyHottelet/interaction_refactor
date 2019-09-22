1edd50f3439431733f9be1cf1da1abe12cb305be
Lily_H <hottelet.alix@gmail.com>
2019-09-22 16:21:49 +0200

    Created separate scripts for close inspection screen

---
+++ Assets/scripts/REFACTOR/CloseInspectionScreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CloseInspectionScreen : GameScreen {
+
+	public float speedX;
+	public float speedY;
+
+    private GameObject currentTargetGo;
+	public Texture2D clickingCursor;
+
+	public override void Initialize(Interactable interactable)
+	{
+        currentTargetGo = interactable.gameObject;
+	}
+
+	void FixedUpdate()
+	{
+
+		if (Input.GetMouseButton (0))
+		{
+			Cursor.SetCursor(clickingCursor, Vector2.zero,CursorMode.Auto);
+			float rotx = Input.GetAxis ("Mouse X") * Time.deltaTime * speedX * Mathf.Deg2Rad;
+			float roty = Input.GetAxis ("Mouse Y") * Time.deltaTime * speedY * Mathf.Deg2Rad;
+            currentTargetGo.transform.RotateAround (Vector2.right, roty);
+            currentTargetGo.transform.RotateAround (Vector2.up, -rotx);
+		}
+		else
+		{
+			Cursor.SetCursor(cursorToDisplay,Vector2.zero,CursorMode.Auto);
+		}
+	}
+}
---
+++ Assets/scripts/REFACTOR/GameScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public abstract class GameScreen : MonoBehaviour
+{
+    public Texture2D cursorToDisplay;
+
+    protected virtual void OnEnable()
+    {
+        //pause game
+        DisplayCustomCursor();
+    }
+
+    protected virtual void OnDisable()
+    {
+        RevertCursor();
+    }
+
+    private void DisplayCustomCursor()
+    {
+        if (cursorToDisplay)
+        {
+            Cursor.SetCursor(cursorToDisplay, Vector2.zero, CursorMode.Auto);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    private void RevertCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public abstract void Initialize(Interactable interactable);
+}
