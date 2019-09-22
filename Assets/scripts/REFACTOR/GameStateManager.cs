77f9a09098312120ea232b297384f68aa0053161
Lily_H <hottelet.alix@gmail.com>
2019-09-22 17:38:59 +0200

    Added a GameStateManager class

---
+++ Assets/scripts/REFACTOR/GameStateManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public enum GameState
+{
+    Default,
+    Paused
+}
+
+public class GameStateManager : MonoBehaviourSingleton<GameStateManager>
+{
+    public GameState currentGameState { get; private set; }
+    public FirstPersonController player;
+
+
+    public void SwitchToState(GameState state)
+    {
+        currentGameState = state;
+    }
+
+    public void SetGamePlaying(bool play)
+    {
+        player.enabled = play;
+        currentGameState = GameState.Paused;
+    }
+}
